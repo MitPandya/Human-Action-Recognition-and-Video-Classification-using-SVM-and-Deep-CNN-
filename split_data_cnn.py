fbcf82909dd4e33626d12b96140b7b904419fd8a
mhpandya <mhpandya@ncsu.edu>
2017-04-16 17:46:55 -0400

    data preprocessing v 1.0

--- split_data_cnn.py
+++ split_data_cnn.py
@@ -2,6 +2,7 @@ import os.path
 import cv2
 import sys
 import os
+from shutil import rmtree
 
 global root_directory
 root_directory = os.getcwd()
@@ -18,9 +19,11 @@ if task not in ['train', 'test']:
 
 def write_frames(file):
 	vidcap = cv2.VideoCapture(read_dir+file.strip())
+	class_ = f.strip().split('/')[0]
+	global classes
+	c = classes[class_]
 	success,image = vidcap.read()
-	cur_class = file.strip().split('/')[0]
-	path = write_dir+cur_class
+	path = write_dir+str(c)
 	if not os.path.exists(path):
 		os.makedirs(path)
 		global count
@@ -37,13 +40,16 @@ def write_frames(file):
 
 read_dir = 'UCF101/'
 if task == 'train':
-	write_dir = 'train/'
+	write_dir = 'classes/'
 	file = open('train_list','r')
 else:
-	write_dir = 'test/'
+	write_dir = 'test_set/'
 	file = open('test_list','r')
 
 global count
+global classes
+classes = {}
+class_count = 0
 count = 0
 c = 0
 if os.path.exists('classes'):
@@ -52,35 +58,12 @@ if os.path.exists('test_set'):
 	rmtree('test_set')
 
 for f in file:
-	c += 1
 	if os.path.exists(read_dir+f.strip()):
+		class_ = f.strip().split('/')[0]
+		if not class_ in classes:
+			classes[class_] = class_count
+			class_count += 1
 		write_frames(f)
   		print "processed %d th video clip." %(c)
+  		c += 1
 file.close()
-
-'''def extract_image(file):
-	vidcap = cv2.VideoCapture(file)
-	success,image = vidcap.read()
-	global count
-	success = True
-	os.chdir(main_dir+'/'+output_dir)
-	print 'writing to ',output_dir
-	while success:
-  		success,image = vidcap.read()
-  		cv2.imwrite("frame%d.png" % count, image)
-  		if cv2.waitKey(10) == 27:
-  			break
-  		count += 1
-try:
-	data_list = os.listdir(video_path)
-	os.mkdir(output_dir)
-	k = 1
-	global count
-	count = 0
-	for files in data_list:
-		os.chdir(main_dir+'/'+video_path)
-		extract_image(files)
-		print "processed %d th video clip." %(k)
-		k += 1
-except:
-	print 'Error writing frames' '''
